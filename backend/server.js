d33cc2191534974adc7d40698ee5de996b23e114
Nokuthula Mahlangu <nokuthulamargaret@gmail.com>
2025-04-27 18:46:49 +0000

    Request and response

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
+require("dotenv").config()
+
 const express = require("express")
 
+//express app
 const app = express()
 
-app.listen(4000,()=>{
-    console.log("Listening to port 4000")
+// routes
+app.get("/", (req,res)=>{
+ res.json({mssg:"Welcome tpo the app"})
+})
+
+// Listening to port 4000
+app.listen(process.env.PORT,()=>{
+    console.log("Listening to port", process.env.PORT)
 })
